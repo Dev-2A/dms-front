7bab659625d7dcface3af227d195ca0c75bf795a
Dev-2A <yehee826@gmail.com>
2023-12-13 19:08:48 +0900

    231213 save3 kosa

--- src/views/components/AuthorsTable.vue
+++ src/views/components/AuthorsTable.vue
@@ -1,46 +1,44 @@
 <template>
   <div class="card">
     <div class="card-header pb-0">
-      <h6>Authors table</h6>
+      <h6>Drowsy Shelter</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
         <table class="table align-items-center mb-0">
           <thead>
             <tr>
-              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Author</th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-10">shelter name</th>
               <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
-              >Function</th>
+                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-10 ps-2"
+              >location</th>
               <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              >Status</th>
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10"
+              >toilet yn</th>
               <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
-              >Employed</th>
-              <th class="text-secondary opacity-7"></th>
+                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-10"
+              >phone number</th>
+              <th class="text-secondary opacity-10"></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr v-if="responseData.data.length > 0">
               <td>
                 <div class="d-flex px-2 py-1">
                   <div>
                     <img
-                      src="../../assets/img/team-2.jpg"
+                      src="../../assets/img/shelters/shelter(1).jpg"
                       class="avatar avatar-sm me-3"
-                      alt="user1"
+                      alt="user2"
                     />
                   </div>
                   <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">John Michael</h6>
-                    <p class="text-xs text-secondary mb-0">john@creative-tim.com</p>
+                    <h6 class="mb-0 text-sm">{{ responseData.data[0].명칭 }}</h6>
                   </div>
                 </div>
               </td>
               <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Organization</p>
+                <p class="text-xs font-weight-bold mb-0">{{ responseData.data[0].노선}}</p>
               </td>
               <td class="align-middle text-center text-sm">
                 <span class="badge badge-sm bg-gradient-success">Online</span>
@@ -240,7 +238,27 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "authors-table",
+  name: "shelter-table",
+  data() {
+    return {
+      responseData: { data: [] }
+    };
+  },
+  mounted() {
+    axios.get('https://api.odcloud.kr/api/15043710/v1/uddi:f49b8c81-af90-41f3-99b7-ed665178731a?page=1&perPage=10&returnType=JSON&serviceKey=FTASscvsf4EMTj1k1CI5zH59G1NenFJ51jTshkstqtQk5Nxpfgd0uPmEy2Wv43H%2FkMMK3GfPXuq24oDc8t3MGw%3D%3D')
+      .then(response => {
+        const dataWithNumberOne = response.data.data.find(item => item.번호 === 1);
+        if (dataWithNumberOne) {
+          this.responseData.data.push(dataWithNumberOne);
+        }
+        console.log(dataWithNumberOne)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
 };
 </script>
